039a71178915e05621c3e2830c359fcfbda8e9f7
adnansamol <samoladnan@gmail.com>
2021-07-02 16:38:04 +0530

    Day2
    Day2

---
+++ Day-2/Day_2_Adnan_Samol.py
@@ -0,0 +1,10 @@
+#Day-2
+a = float(input("Enter the number to find cube root:"))
+125
+def cubeRoot(a):
+    if(a == 0):
+        return 0;
+    else:
+        return (a**(1/3))   
+
+print("The cube root of",int(a), "is",cubeRoot(a))
