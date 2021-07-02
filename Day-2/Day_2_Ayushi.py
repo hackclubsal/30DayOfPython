22f1a2ead6d8eb7dbd2abe310f0472dc4d9f6a77
Ayushi <binaashi99@gmail.com>
2021-07-02 12:57:15 -0500

    Day 2 commit

---
+++ Day-2/Day_2_Ayushi.py
@@ -0,0 +1,22 @@
+#Day 2 Challenge
+
+#-> Question: You are given a variable a. Write a program to find the cube root of a number.
+
+#-> Input: Take input from the user and store it into the given variable.
+#-> Output: Print the cube root of the number stored in variable a.
+
+#-> Example
+#Input: a = 125
+#Output: 5 
+
+
+import math
+a=int(input())
+if a>=0:
+    print(f"Cube root of {a} is {a**(1/3)}")
+    print(f"Cube root of {a} upto 3 decimal places is {round(a**(1/3),3)}")
+    print(f"Cube root of {a} in ceiling form is {math.ceil(a**(1/3))}")
+if a<0:
+    print(f"Cube root of {a} is {-(-a)**(1/3)}")
+    print(f"Cube root of {a} upto 3 decimal places is {round(-(-a)**(1/3),3)}")
+    print(f"Cube root of {a} in ceiling form is {math.ceil(-(-a)**(1/3))}")
