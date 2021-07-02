6f5aca5837323f1472a252f92295b6cea4aba4b5
HackClub SAL <83156459+hackclubsal@users.noreply.github.com>
2021-07-02 17:07:00 +0530

    Create day-2_solution_tejas.py

---
+++ Day-2/day-2_solution_tejas.py
@@ -0,0 +1,8 @@
+a = float(input("Enter the number to find cube root:"))
+def cubeRoot(a):
+    if(a == 0):
+        return 0;
+    else:
+        return (a**(1/3))   
+
+print("The cube root of",int(a), "is",cubeRoot(a))
