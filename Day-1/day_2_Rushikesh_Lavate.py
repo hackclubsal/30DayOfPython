735cab96224b495c860437139c2a9602ad5030a0
Rushikesh Lavate <61430438+Rushi21-kesh@users.noreply.github.com>
2021-07-02 10:22:02 +0530

    Add files via upload

---
+++ Day-1/day_2_Rushikesh_Lavate.py
@@ -0,0 +1,17 @@
+class cude_root:
+    def __init__(self,num):
+        self.num = num
+
+    def find_c_root(self):
+        if self.num > 0:
+            result = self.num**(1/3)
+        elif self.num < 0:
+            result = (-(-self.num)**(1/3))
+        else:
+            result = (self.num)
+
+        print('Cude root of number {} is {}'.format(self.num, result))
+
+num = int(input("Enter the cude: "))
+obj = cude_root(num=0)
+obj.find_c_root()
