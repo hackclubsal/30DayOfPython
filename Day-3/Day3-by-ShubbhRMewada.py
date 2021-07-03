c56b4a4315ea2c981be5a2b44e80b158a3df1a84
ShubbhRMewada <86721912+ShubbhRMewada@users.noreply.github.com>
2021-07-03 10:34:38 +0530

    Add files via upload

---
+++ Day-3/Day3-by-ShubbhRMewada.py
@@ -0,0 +1,6 @@
+n=int(input('Enter a Number: '))
+if(n%10==0 and n%20==0):
+    print(f'Yes,the number {n} is divisible by both 10 and 20')
+else:
+    print(f'No,the number {n} is not divisible by both 10 and 20')
+    
