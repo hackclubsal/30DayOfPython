ee049d7d9fef79766035e2ddcb6fe2074561c895
Ayushi <binaashi99@gmail.com>
2021-07-04 12:32:38 -0500

    Day 4 Commit

--- Day-4/Day_4_Ayushi.py
+++ Day-4/Day_4_Ayushi.py
@@ -18,13 +18,13 @@
 
 n=int(input())
 
-print("-----Method 1 : Explicitly Counting-----")
+print("-----Method 1 : Explicitly Counting -----")
 for i in range(n):
     print("* "*(i+1), end=" ")
     print()
 print(f"Number of Stars : {(n*(n+1))//2}")
 
-print("-----Method 2 : Step wise counting-----")
+print("-----Method 2 : Step wise counting -----")
 count=0
 for i in range(1,n+1):
     for j in range(1,i+1):
