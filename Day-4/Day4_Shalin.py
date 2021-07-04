f0deb9f333bffb64910a97dcd7f4ca57397ed4b8
Shalin Bhavsar <35373379+shalin24999@users.noreply.github.com>
2021-07-04 22:10:45 +0530

    Solution of Day4

---
+++ Day-4/Day4_Shalin.py
@@ -0,0 +1,4 @@
+for i in range(1, int(input("Enter the number of rows:")) +1): #Getting input and running for loop for rows
+    for j in range(0, i): #for loop for columns
+        print("* ", end="")
+    print() 
