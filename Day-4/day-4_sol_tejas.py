f3c5f48c15999eff3a5c6ba009195f40994082d2
HackClub SAL <83156459+hackclubsal@users.noreply.github.com>
2021-07-04 16:19:15 +0530

    Update day-4_sol_tejas.py

--- Day-4/day-4_sol_tejas.py
+++ Day-4/day-4_sol_tejas.py
@@ -1,7 +1,13 @@
 n = int(input("enter the rows you want to print: "))
 
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print("* ", end="")
-    print("\n")
+def pyramid(n):
+    star = 0
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            star++
+            print("* ", end="")
+        print("\n")
+    print(star)
+    
+pyramid(n)    
     
