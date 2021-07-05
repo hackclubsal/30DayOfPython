413e54edb3580939b864c8b749a565c8126ef932
Yukta-Gurnani <yuktagurnani0902@gmail.com>
2021-07-05 16:59:46 +0500

    Day-3_Solution Yukta Gurnani

---
+++ Day-3/Day-3_Yukta.py
@@ -0,0 +1,7 @@
+x =(int)( input("Enter a number: "))
+if(x % 10 == 0 and x % 20 == 0  ):
+    print('yes, {}  is divisible by both 10 & 20'.format(x))
+else:
+    print('yes, {}  is not divisible by both 10 & 20'.format(x))
+             
+   
