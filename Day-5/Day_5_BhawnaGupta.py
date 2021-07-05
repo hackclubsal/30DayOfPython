8e50a8e1faa96fd967a8b8de23150790e0f23480
Bhawna Gupta <onlybhawnagupta@gmail.com>
2021-07-05 21:01:48 +0530

    Day_5_Solution_BhawnaGupta

---
+++ Day-5/Day_5_BhawnaGupta.py
@@ -0,0 +1,12 @@
+list1 = [1, 3, 5, 7, 8, 10,12]
+list2 = [4, 6, 9, 11]
+list3 = [2]
+Month = int(input("Enter the Month Number : "))
+if Month in list1:
+    print(31)
+elif Month in list2:
+    print(30)
+elif Month in list3:
+    print(28)
+else:
+    print("Invalid Month Number")
