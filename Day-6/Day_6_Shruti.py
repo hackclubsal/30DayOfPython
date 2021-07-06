40e505b968441a73042212dcd465f3489c7af44c
ShrutiLad242 <82631455+ShrutiLad242@users.noreply.github.com>
2021-07-06 21:09:18 +0530

    Day_6_Solution_Shruti

---
+++ Day-6/Day_6_Shruti.py
@@ -0,0 +1,10 @@
+x=input("enter character")
+print("ASCII value:",ord(x))
+y=str(bin(ord(x)))
+print("Binary value:",y[2:])
+'''
+Output:
+Enter character:a
+ASCII value:97
+Binary value:1100001
+'''
