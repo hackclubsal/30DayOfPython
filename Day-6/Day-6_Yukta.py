9a3f4d691367d09014743765656b8c0cb93d2580
Yukta-Gurnani <yuktagurnani0902@gmail.com>
2021-07-06 10:45:45 +0500

    Day-6_Solution Yukta Gurnani

---
+++ Day-6/Day-6_Yukta.py
@@ -0,0 +1,11 @@
+ch = input("Enter a character : ")
+print("ASCII value of "+ ch +" is : ",ord(ch))
+
+print("Binary value of ", ch, " is : ", bin(ord(ch)))
+'''
+OUTPUT -
+Enter a character : A
+ASCII value of A is :  65
+Binary value of  A  is :  0b1000001
+'''
+
