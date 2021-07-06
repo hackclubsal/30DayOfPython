061e0b01db2f2957c581b238c895e4f51aa04b80
Mohit Shah <69730742+msMohit20@users.noreply.github.com>
2021-07-06 13:53:18 +0530

    Day_6_Mohit_Solution

---
+++ Day-6/Day_6_Mohit.py
@@ -0,0 +1,5 @@
+# Write a program to find ascii code of the given input character.
+
+i = input("Enter the value whose ASCII value you want to know : ")
+
+print(ord(i), "is the ASCII value of ", i, ".")
