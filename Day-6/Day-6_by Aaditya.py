5128f03002420ddde4f6770222856eaa21c92668
aadii0408 <83656297+aadii0408@users.noreply.github.com>
2021-07-07 15:58:58 +0530

    Create Day-6_by Aaditya.py

---
+++ Day-6/Day-6_by Aaditya.py
@@ -0,0 +1,5 @@
+#day6_challenge
+
+ch = input("Enter any character: ")
+print("The ASCII value of char " + ch + " is: ",ord(ch)) 
+ #ord func() ,we can use this function to find the ASCII value of any character.
