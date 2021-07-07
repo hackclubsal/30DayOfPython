7c372a0ba6869436ea6c9c7da520da0ae15ce1ce
premkumar0 <premkumarnunna@gmail.com>
2021-07-07 16:10:58 +0000

    day_6_solution_Premkumar

---
+++ Day-6/day_6_Premkumar.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+  ch = input("Enter a charecter \n")
+  print("ASCII code for the entered charecter is :- {}".format(ord(ch)))
+  print("Binary value for the ASCII code is :- {}".format(bin(ord(ch))))
