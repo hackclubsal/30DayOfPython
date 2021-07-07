4185a0da695a2cba378f2e7b10bc20b6ccf81860
HackClub SAL <83156459+hackclubsal@users.noreply.github.com>
2021-07-07 13:34:21 +0530

    Delete Day_5_Sneha.py

--- Day5/Day_5_Sneha.py
+++
@@ -1,3 +0,0 @@
-n= (input())
-month={'1':31, '2':'29 or 28 according to year', '3':31, '4':30, '5':31, '6':30, '7':31, '8':31, '9':30, '10':31, '11':30, '12':31}
-print("Number of days in given month is:",month[n])
