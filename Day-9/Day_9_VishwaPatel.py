745e25a94aac1c36fc7ba3b46acf19713cf9d387
VishwaPatel0311 <86764526+VishwaPatel0311@users.noreply.github.com>
2021-07-09 11:19:43 +0530

    Day_9_VishwaPatel.py

---
+++ Day-9/Day_9_VishwaPatel.py
@@ -0,0 +1,13 @@
+import math as m
+
+num = int(input('Enter number'))
+print("Last digit of {} is {}".format(num, num % 10))
+
+num = str(num)
+length = len(num)
+
+if (length % 2 == 0):
+    print(-1)
+else: 
+    mid = m.floor(length/ 2)
+    print("Middle digit of {} is {}".format(num, num[mid]))
