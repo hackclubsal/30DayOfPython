291c44dbef5f8aa493e5eebfc46979c6d0eaa6d7
FreyaBavisi <86787218+FreyaBavisi@users.noreply.github.com>
2021-07-11 11:28:54 +0530

    Day_11_Solution_Freya

---
+++ Day-11/Day_11_Freya.py
@@ -0,0 +1,10 @@
+# to print nth element of fibonacci series
+n=int(input("Enter n"))
+t1=1
+t2=0
+print("nth element of Fibonacci series:",end=" ")
+for i in range(1,n+1):
+    t3=t1+t2
+    t1=t2
+    t2=t3
+print(t3)
