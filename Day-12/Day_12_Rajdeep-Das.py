acc6d0b57333f45e58e2ff95d444f87228fe843d
Rajdeep Das <44817007+Rajspeaks@users.noreply.github.com>
2021-07-12 22:19:35 +0530

    Add files via upload

---
+++ Day-12/Day_12_Rajdeep-Das.py
@@ -0,0 +1,13 @@
+def linearsearch(arr, x):
+   for i in range(len(arr)):
+      if arr[i] == x:
+         return i
+   return -1
+arr = [1,2,3,4,5,6,7,8]
+x = 4
+print("element found at index "+str(linearsearch(arr,x)))
+
+
+
+
+//output: element found at index 3
