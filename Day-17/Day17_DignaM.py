05669d60d0d3ab5f6608786f2f879fac4cdf3096
DignaM1008 <vaibhavmistri4@gmail.com>
2021-07-19 21:25:38 +0530

    Added Day17-18 Solution

---
+++ Day-17/Day17_DignaM.py
@@ -0,0 +1,19 @@
+# Day-17_Hackclubsal_30DayOfPython_Solution
+# Program to Implement a Queue in Python
+
+def create_queue(queue):
+    queue = []
+def enqueue(stack, a):
+    stack.append(a)
+def dequeue(stack):
+    stack.pop(0)
+def prqueue(queue):
+    print(queue)
+
+q = []
+
+enqueue(q, 9)
+enqueue(q, 7)
+dequeue(q)
+enqueue(q, 8)
+prqueue(q)
