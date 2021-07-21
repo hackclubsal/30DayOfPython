63be966b0992a86f7abb11cc231e057e3567b9a7
AJAYRAJ NADAR <77918365+Ajayraj0911@users.noreply.github.com>
2021-07-21 18:59:12 +0530

    Day_17_Solution_AjayrajNadar

---
+++ Day-17/Day_17_AjayrajNadar.py
@@ -0,0 +1,15 @@
+def create(queue):
+    queue = []
+def en(stack, a):
+    stack.append(a)
+def de(stack):
+    stack.pop(0)
+def prqueue(queue):
+    print(queue)
+q = []
+
+en(q, 9)
+en(q, 7)
+de(q)
+en(q, 8)
+pr(q)
