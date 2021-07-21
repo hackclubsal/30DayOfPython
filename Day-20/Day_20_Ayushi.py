b8b9d2b50bdf670ee407b0ba262cb6da24ea384f
Ayushi <binaashi99@gmail.com>
2021-07-21 23:31:18 +0530

    day 20 commit

---
+++ Day-20/Day_20_Ayushi.py
@@ -0,0 +1,15 @@
+# Day-20 Question for the day-20 is --->
+# -> Question: Write a program that takes a string as an input and convert it's all first latter of words into Uppercase.
+
+# -> Input: 
+# dogs and cats
+# -> Output: 
+# Dogs And Cats
+
+# -> Input:- 
+# nothing is here
+# -> output:- 
+# Nothing Is Here
+
+n=input()
+print(n.title())
