3facf4a4fb21aa25d6747031779fb0eac1e006bb
Mohit Shah <69730742+msMohit20@users.noreply.github.com>
2021-07-23 23:09:20 +0530

    Update Day_23_Mohit.py

--- Day-23/Day_23_Mohit.py
+++ Day-23/Day_23_Mohit.py
@@ -1,43 +1,19 @@
-# Python3 program to construct tree from Array
-class newNode:
-    def __init__(self, data):
-        self.data = data
-        self.left = self.right = None
+
+def solution(A, K):
+    old = A
+    new = [0]*len(A)
+    for i in range(K):
+        new[0] = old[-1]
+        new[1:] = old[:-1]
+        old = new.copy()  # This was the problematic line
+    return new
 
 
-
-def insertLevelOrder(arr, root, i, n):
-
-    if i < n:
-        temp = newNode(arr[i])
-        root = temp
-
-        # insert left child
-        root.left = insertLevelOrder(arr, root.left,
-                                     2 * i + 1, n)
-
-        # insert right child
-        root.right = insertLevelOrder(arr, root.right,
-                                      2 * i + 2, n)
-    return root
-
-# Function to print tree nodes
-
-def inOrder(root):
-    if root != None:
-        inOrder(root.left)
-        print(root.data, end=" ")
-        inOrder(root.right)
-
-
-arr = []
 n = int(input("Enter the Elements Range : "))
+k = int(input("enter number of Rotation : "))
+arr = []
 for i in range(0, n):
     ele = int(input("Element : "))
     arr.append(ele)
-
-print("Array : ", arr)
-n = len(arr)
-root = None
-root = insertLevelOrder(arr, root, 0, n)
-inOrder(root)
+print(arr, " is the first inserted Array.")
+print(solution(arr, k), "is the rotated Array.")
