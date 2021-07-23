ddd680e162f900af7fbfd60666d52b34c0eedcd2
Ayushi <binaashi99@gmail.com>
2021-07-23 22:55:38 +0530

    commit 23

---
+++ Day-21/Dy_21_Ayushi.py
@@ -0,0 +1,11 @@
+# Day-21 Question for the day-21 is --->
+# -> Question: Implement a Queue with the help of two stacks in Python 
+
+# -> Input: 
+# enqueue(10)
+# enqueue(11)
+# enqueue(12)
+# -> Output: 
+# 10
+# 11
+# 12
