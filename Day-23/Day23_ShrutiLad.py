ae03375026950dcdce135170b881062d2f4fac75
ShrutiLad242 <82631455+ShrutiLad242@users.noreply.github.com>
2021-07-23 23:04:09 +0530

    Day23_shrutiLad_solution

---
+++ Day-23/Day23_ShrutiLad.py
@@ -0,0 +1,12 @@
+n=int(input("enter size of array"))
+array=[]
+for i in range(0,n):
+    x=int(input("enter element"))
+    array.append(x)
+print("Before rotation:",array)
+def rotate(array):
+   y=array.pop()
+   array.insert(0,y)
+for i in range(2): #to perform rotation twice
+ rotate(array)
+print("After two rotations:",array)
