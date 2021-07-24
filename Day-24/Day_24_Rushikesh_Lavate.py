fa98e8c491ee6c8915971a0a7ee05e47b52a2854
Rushikesh Lavate <61430438+Rushi21-kesh@users.noreply.github.com>
2021-07-24 14:17:12 +0530

    Day_24_Solution_Rushikesh_Lavate

---
+++ Day-24/Day_24_Rushikesh_Lavate.py
@@ -0,0 +1,17 @@
+#30 Days Of Python
+
+Day-24 Question for the day-24 is --->
+
+-> Question:  Write a program that validates email address with the help of regular expressions
+
+-> Input: 
+Email= abs@gmail.com
+
+-> Output:-
+Valid Email!
+
+-> Input:-
+Email = g33n.com
+
+-> Output:- 
+Invalid Email!
