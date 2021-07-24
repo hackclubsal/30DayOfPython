0e35ac4dbb84a076d17eee78682a6b895dd2df5a
VishwaPatel0311 <86764526+VishwaPatel0311@users.noreply.github.com>
2021-07-24 21:23:49 +0530

    Day_24_VishwaPatel.py

---
+++ Day-24/Day_24_VishwaPatel.py
@@ -0,0 +1,11 @@
+import re
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+ 
+def check(email):
+    if(re.match(regex, email)):
+        print("Valid Email")
+ 
+    else:
+        print("Invalid Email")
+ 
+check(input('Enter email:'))
