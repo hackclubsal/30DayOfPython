1c2e88a45afae3dfd2cd3b98e8e481a766dbb684
Mohit Shah <69730742+msMohit20@users.noreply.github.com>
2021-07-27 18:23:21 +0530

    Day_26_Solution_Mohit

--- Day-26/Day_26_Mohit.py
+++ Day-26/Day_26_Mohit.py
@@ -1,37 +1,37 @@
 # Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
-import math
-import os
-import random
-import re
-import sys
+def maxSubOn3(arr):
+
+    maximum = 0
+    for i in range(n):
+        for j in range(i, n):
+            subsum = 0
+            for k in range(i, j):
+                subsum += arr[k]
+            maximum = max(subsum, maximum)
+    return maximum
 
 
-def maxSubarray(arr):
-    # Subset
-    arr1 = sorted(arr, reverse=True)
-    subs = arr1[0]
-    i = 1
-    while i < len(arr1) and subs + arr1[i] > subs:
-        subs += arr1[i]
-        i += 1
-    # Subarray
-    MSS = [0]*len(arr)
-    MSS[0] = arr[0]
-    for i in range(1, len(arr)):
-        MSS[i] = max(arr[i], MSS[i-1] + arr[i])
-
-    return max(MSS)
+def maxSubOn2(arr):
+    maximum = 0
+    for i in range(n):
+        subsum = 0
+        for j in range(i, n):
+            subsum += arr[j]
+            maximum = max(maximum, subsum)
+    return maximum
 
 
 if __name__ == '__main__':
-
-    t = int(input("Enter :"))
-
-    for i in range(t):
-        n = int(input())
-
-        arr = list(map(int, input()))
-
-        result = maxSubarray(arr)
-
-print(result, "is the answer.")
+    n = int(input("Enter the size of array:- "))
+    intArray = []
+    print("Enter Element of integer list ")
+    for i in range(n):
+        ele = int(input())
+        intArray.append(ele)
+    option = input(
+        "Enter on2 for O(n^2) and on3 for O(n^3) time complexity Subarray:- ")
+    if(option == 'on2'):
+        maxsum = maxSubOn2(intArray)
+    elif(option == 'on3'):
+        maxsum = maxSubOn3(intArray)
+    print("maximum sum of Subarray is {}".format(maxsum))
