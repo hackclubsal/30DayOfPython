9b673797e0d634ba192b4ef5835fa9b2946b4550
ShrutiLad242 <82631455+ShrutiLad242@users.noreply.github.com>
2021-07-27 21:52:21 +0530

    Day27_ShrutiLad_Solution

---
+++ Day-27/Day27_ShrutiLad.py
@@ -0,0 +1,35 @@
+def areBracketsBalanced(expr):
+	stack = []
+
+	# Traversing the Expression
+	for char in expr:
+		if char in ["(", "{", "["]:
+			stack.append(char)
+		else:
+
+			if not stack:
+				return False
+			current_char = stack.pop()
+			if current_char == '(':
+				if char != ")":
+					return False
+			if current_char == '{':
+				if char != "}":
+					return False
+			if current_char == '[':
+				if char != "]":
+					return False
+
+	
+	if stack:
+		return False
+	return True
+
+
+expr = input("Enter a string:")
+
+	
+if areBracketsBalanced(expr):
+	print("True")
+else:
+	print("False")
